532645daf1bf970d4e795d44ba8f279d47143ac3
darkphooo <dark.xiaoyao@live.com>
2016-04-01 23:56:31 +0800

    2016/04/01 complete

---
+++ js/over.js
@@ -0,0 +1,29 @@
+function highschOver(obj)
+{
+  document.getElementById('highschico').className = 'icon-caret-down';
+}
+
+function highschOut(obj)
+{
+  document.getElementById('highschico').className = 'icon-caret-right';
+}
+
+function collegeOver(obj)
+{
+  document.getElementById('collegeico').className = 'icon-caret-down';
+}
+
+function collegeOut(obj)
+{
+  document.getElementById('collegeico').className = 'icon-caret-right';
+}
+
+function work1Over(obj)
+{
+  document.getElementById('work1ico').className = 'icon-caret-down';
+}
+
+function work1Out(obj)
+{
+  document.getElementById('work1ico').className = 'icon-caret-right';
+}
