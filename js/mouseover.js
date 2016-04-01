42b90d7e7ac7b6be2a4b480481ebfbb3c77ac41c
darkphooo <dark.xiaoyao@live.com>
2016-04-01 23:08:52 +0800

    update

---
+++ js/mouseover.js
@@ -0,0 +1,9 @@
+function highschOver(obj)
+{
+  document.getElementById("hide-highsch").style.display="block";
+}
+
+function collegeOver(obj)
+{
+  document.getElementById("hide-college").style.display="block";
+}
