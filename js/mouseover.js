8ee4dd462312d63dc0828323bf74e483c5028a94
darkphooo <dark.xiaoyao@live.com>
2016-04-01 23:15:25 +0800

    update

--- js/mouseover.js
+++
@@ -1,9 +0,0 @@
-function highschOver(obj)
-{
-  document.getElementById("hide-highsch").style.display="block";
-}
-
-function collegeOver(obj)
-{
-  document.getElementById("hide-college").style.display="block";
-}
